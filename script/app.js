538336f83e737dafce655f3c595190bde50c3fbb
Piyas <piyas62552@gmail.com>
2026-03-08 09:57:34 +0600

    API Link Add

---
+++ script/app.js
@@ -0,0 +1,6 @@
+const ALL_ISSUES_API = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+const SEARCH_API =
+  "https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=";
+const SINGLE_ISSUE_API = "https://phi-lab-server.vercel.app/api/v1/lab/issue/";
+
+let allIssues = [];
